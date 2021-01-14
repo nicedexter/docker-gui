537dfb34f40230df24ff9dd1fe5a03323f5ba4ec
Manuel Spuhler <Manuel.Spuhler@chuv.ch>
2021-01-14 12:06:06 +0100

    blender on 4040

--- app/build.sh
+++ app/build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-docker stop gimp-web
-docker stop gimp-app
-docker rm gimp-web
-docker rm gimp-app
-docker network rm gimp-net
-docker volume rm gimp-data
+docker stop blender-web
+docker stop blender-app
+docker rm blender-web
+docker rm blender-app
+docker network rm blender-net
+docker volume rm blender-data
 
-docker build -t gimp .
-docker network create gimp-net
-docker volume create gimp-data
+docker build -t blender .
+docker network create blender-net
+docker volume create blender-data
 
-docker run --detach --restart=always --volume=gimp-data:/data --net=gimp-net --name=gimp-app gimp
+docker run --detach --restart=always --volume=blender-data:/data --net=blender-net --name=blender-app blender
 
 cd caddy
-docker build -t gimp-caddy .
-APP_PASSWORD_HASH=$(docker run --rm -it gimp-caddy caddy hash-password -plaintext 'password')
+docker build -t blender-caddy .
+APP_PASSWORD_HASH=$(docker run --rm -it blender-caddy caddy hash-password -plaintext 'password')
 
-docker run --detach --restart=always --volume=gimp-data:/data --net=gimp-net --name=gimp-web --env=APP_USERNAME="user" --env=APP_PASSWORD_HASH="${APP_PASSWORD_HASH}" --publish=8080:8080 gimp-caddy
+docker run --detach --restart=always --volume=blender-data:/data --net=blender-net --name=blender-web --env=APP_USERNAME="user" --env=APP_PASSWORD_HASH="${APP_PASSWORD_HASH}" --publish=4040:4040 blender-caddy
