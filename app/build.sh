03f97a3af60f1d6c9f2d701d05d515017abd18e1
nicedexter <manuel.spuhler@gmail.com>
2021-01-14 12:08:13 +0000

    docker build --network=host for CSCS

--- app/build.sh
+++ app/build.sh
@@ -7,14 +7,14 @@ docker rm blender-app
 docker network rm blender-net
 docker volume rm blender-data
 
-docker build -t blender .
+docker build -t blender --network=host .
 docker network create blender-net
 docker volume create blender-data
 
 docker run --detach --restart=always --volume=blender-data:/data --net=blender-net --name=blender-app blender
 
 cd caddy
-docker build -t blender-caddy .
+docker build -t blender-caddy --network=host .
 APP_PASSWORD_HASH=$(docker run --rm -it blender-caddy caddy hash-password -plaintext 'password')
 
 docker run --detach --restart=always --volume=blender-data:/data --net=blender-net --name=blender-web --env=APP_USERNAME="user" --env=APP_PASSWORD_HASH="${APP_PASSWORD_HASH}" --publish=4040:4040 blender-caddy
