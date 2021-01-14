bd9b054d4222786ba1be062de648d645a8377e5d
Manuel Spuhler <Manuel.Spuhler@chuv.ch>
2021-01-14 16:36:56 +0100

    flatpack test

--- app/build.sh
+++ app/build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-docker stop blender-web
-docker stop blender-app
-docker rm blender-web
-docker rm blender-app
-docker network rm blender-net
-docker volume rm blender-data
+docker stop jasp-web
+docker stop jasp-app
+docker rm jasp-web
+docker rm jasp-app
+docker network rm jasp-net
+docker volume rm jasp-data
 
-docker build -t blender --network=host .
-docker network create blender-net
-docker volume create blender-data
+docker build -t jasp --network=host .
+docker network create jasp-net
+docker volume create jasp-data
 
-docker run --detach --restart=always --volume=blender-data:/data --net=blender-net --name=blender-app blender
+docker run --detach --restart=always --volume=jasp-data:/data --net=jasp-net --name=jasp-app jasp
 
 cd caddy
-docker build -t blender-caddy --network=host .
-APP_PASSWORD_HASH=$(docker run --rm -it blender-caddy caddy hash-password -plaintext 'password')
+docker build -t jasp-caddy --network=host .
+APP_PASSWORD_HASH=$(docker run --rm -it jasp-caddy caddy hash-password -plaintext 'password')
 
-docker run --detach --restart=always --volume=blender-data:/data --net=blender-net --name=blender-web --env=APP_USERNAME="user" --env=APP_PASSWORD_HASH="${APP_PASSWORD_HASH}" --publish=4040:4040 blender-caddy
+docker run --detach --restart=always --volume=jasp-data:/data --net=jasp-net --name=jasp-web --env=APP_USERNAME="user" --env=APP_PASSWORD_HASH="${APP_PASSWORD_HASH}" --publish=3030:3030 jasp-caddy
